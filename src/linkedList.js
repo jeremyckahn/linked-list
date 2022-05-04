5b500504bb411bc836b01ea8bb7b39d245192865
Jeremy Kahn <jeremyckahn@gmail.com>
2022-05-03 21:57:13 -0500

    chore: unimplement functions

--- src/linkedList.js
+++ src/linkedList.js
@@ -35,22 +35,7 @@ class LinkedList {
    * @returns {LinkedListNode}
    */
   insertLast(value, startingNode) {
-    if (this.isEmpty()) {
-      return this.insertFirst(value);
-    }
-
-    if (startingNode && !(startingNode instanceof LinkedListNode)) {
-      throw new Error('insertLast expects a LinkedListNode starting node');
-    }
-
-    let current = startingNode || this._head;
-    while (current.hasNext()) {
-      current = current.getNext();
-    }
-
-    current.setNext(new LinkedListNode(value, null));
-    this._count += 1;
-    return current.getNext();
+    // TODO: Implement this function
   }
 
   /**
@@ -61,29 +46,7 @@ class LinkedList {
    * @returns {LinkedListNode}
    */
   insertAt(position, value) {
-    if (
-      Number.isNaN(+position)
-      || position < 0 || position > this._count
-    ) {
-      throw new Error('.insertAt expects a position num <= linked list size');
-    }
-
-    // head node is at position 0
-    if (position === 0) {
-      return this.insertFirst(value);
-    }
-
-    let currentPosition = 1;
-    let prev = this._head;
-    while (currentPosition < position) {
-      currentPosition += 1;
-      prev = prev.getNext();
-    }
-
-    // add it at a position after the head, between prev & prev.getNext()
-    prev.setNext(new LinkedListNode(value, prev.getNext()));
-    this._count += 1;
-    return prev.getNext();
+    // TODO: Implement this function
   }
 
   /**
@@ -92,12 +55,7 @@ class LinkedList {
    * @returns {LinkedListNode}
    */
   removeFirst() {
-    if (this.isEmpty()) return null;
-
-    const removed = this._head;
-    this._head = this._head.getNext();
-    this._count -= 1;
-    return removed.setNext(null);
+    // TODO: Implement this function
   }
 
   /**
@@ -106,23 +64,7 @@ class LinkedList {
    * @returns {LinkedListNode}
    */
   removeLast() {
-    if (this.isEmpty()) return null;
-
-    let prev = null;
-    let current = this._head;
-    while (current.hasNext()) {
-      prev = current;
-      current = current.getNext();
-    }
-
-    // linked list has 1 node
-    if (prev === null) {
-      return this.removeFirst();
-    }
-
-    prev.setNext(null);
-    this._count -= 1;
-    return current;
+    // TODO: Implement this function
   }
 
   /**
@@ -132,30 +74,7 @@ class LinkedList {
    * @returns {number} number of removed nodes
    */
   removeEach(cb) {
-    if (typeof cb !== 'function') {
-      throw new Error('.removeEach(cb) expects a callback');
-    }
-
-    let removedCount = 0;
-    let position = 0;
-    let prev = null;
-    let current = this._head;
-    while (current instanceof LinkedListNode) {
-      if (cb(current, position)) {
-        if (prev === null) {
-          this.removeFirst();
-        } else {
-          prev.setNext(prev.getNext().getNext());
-        }
-        this._count -= 1;
-        removedCount += 1;
-      } else {
-        prev = current;
-      }
-      position += 1;
-      current = current.getNext();
-    }
-    return removedCount;
+    // TODO: Implement this function
   }
 
   /**
@@ -165,28 +84,7 @@ class LinkedList {
    * @returns {LinkedListNode}
    */
   removeAt(position) {
-    if (
-      Number.isNaN(+position)
-      || position < 0
-      || position >= this._count
-    ) {
-      return null;
-    }
-
-    if (position === 0) {
-      return this.removeFirst();
-    }
-
-    let counter = 1;
-    let prev = this._head;
-    while (counter < position) {
-      counter += 1;
-      prev = prev.getNext();
-    }
-    const removed = prev.getNext();
-    prev.setNext(prev.getNext().getNext());
-    this._count -= 1;
-    return removed.setNext(null);
+    // TODO: Implement this function
   }
 
   /**
@@ -195,17 +93,7 @@ class LinkedList {
    * @param {function} cb
    */
   forEach(cb) {
-    if (typeof cb !== 'function') {
-      throw new Error('.forEach(cb) expects a callback');
-    }
-
-    let current = this._head;
-    let position = 0;
-    while (current instanceof LinkedListNode) {
-      cb(current, position);
-      position += 1;
-      current = current.getNext();
-    }
+    // TODO: Implement this function
   }
 
   /**
@@ -216,22 +104,7 @@ class LinkedList {
    * @returns {LinkedListNode}
    */
   find(cb, startingNode = this._head) {
-    if (typeof cb !== 'function') {
-      throw new Error('.find(cb) expects a callback');
-    }
-
-    if (startingNode && !(startingNode instanceof LinkedListNode)) {
-      throw new Error('.find(cb) expects to start from a LinkedListNode');
-    }
-
-    let current = startingNode;
-    while (current instanceof LinkedListNode) {
-      if (cb(current)) {
-        return current;
-      }
-      current = current.getNext();
-    }
-    return null;
+    // TODO: Implement this function
   }
 
   /**
@@ -241,17 +114,7 @@ class LinkedList {
    * @returns {LinkedList}
    */
   filter(cb) {
-    if (typeof cb !== 'function') {
-      throw new Error('.filter(cb) expects a callback');
-    }
-
-    let last = null;
-    const result = new LinkedList();
-    this.forEach((node, position) => {
-      if (!cb(node, position)) return;
-      last = result.insertLast(node.getValue(), last);
-    });
-    return result;
+    // TODO: Implement this function
   }
 
   /**
